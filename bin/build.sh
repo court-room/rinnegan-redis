55c0e8ceea913910f01a617b57a8d9b7aab1a7d1
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2020-09-05 22:45:38 +0530

    First release candidate (#9)

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 clear
-docker-compose build --compress --force-rm
+
+docker build  --compress --force-rm --tag rinnegan-redis:latest .
 
 docker image tag rinnegan-redis:latest localhost:6000/rinnegan-redis:latest
 
