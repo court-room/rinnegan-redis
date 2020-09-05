7774b60f731fbdfb277cd15de4871c3a83712cc2
onlinejudge95 <onlinejudge95@gmail.com>
2020-09-05 13:53:19 +0530

    Adds build step and push to registry

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,6 @@
 clear
 docker-compose build --compress --force-rm
 
-docker image tag rinnegan-postgres:latest localhost:6000/rinnegan-redis:latest
+docker image tag rinnegan-redis:latest localhost:6000/rinnegan-redis:latest
 
 docker push localhost:6000/rinnegan-redis:latest
